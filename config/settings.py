bf435dc15db77511965603613105ed0e4243aff3
jihuncho7 <72445485+jihuncho7@users.noreply.github.com>
2021-02-20 00:37:42 +0900

    add ALLOWED_HOST IP

--- config/settings.py
+++ config/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'pcrlezvm6%a97_#81iffy3p1ckwc^*cwo1p+thhpeo#k)iej#c'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['13.124.47.48']
 
 
 # Application definition
